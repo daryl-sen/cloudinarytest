7263ef59cde93ea030dbae7a37f8a9ce70487ef2
Daryl <tsdaryl@gmail.com>
2020-10-31 18:03:45 -0700

    Added .env template

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 
 from dotenv import load_dotenv
 import os
-# import cloudinary
-# import cloudinary.uploader
-# import cloudinary.api
+
 load_dotenv()
 
 cloudinary_vars = {
@@ -14,15 +12,11 @@ cloudinary_vars = {
     'CLOUDINARY_API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
 }
 
-# cloudinary.config( 
-#   cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'), 
-#   api_key = os.getenv('CLOUDINARY_API_KEY'), 
-#   api_secret = os.getenv('CLOUDINARY_API_SECRET')
-# )
-
-# print('key')
-# print(os.getenv('CLOUDINARY_API_KEY'))
-# print(cloudinary_vars['CLOUDINARY_CLOUD_NAME'])
+# inside .env
+# export SECRET_KEY=value
+# export CLOUDINARY_API_KEY=value
+# export CLOUDINARY_API_SECRET=value
+# export CLOUDINARY_NAME=value
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
